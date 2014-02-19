1d89daa45e0c4c75aa8a45ce5412a48d2b78185d
Valery Savich <Relrin78@gmail.com>
2014-02-19 22:28:05 +0300

    Module with supported commands for server/client/file server

---
+++ helenae/commands.py
@@ -0,0 +1,78 @@
+"""
+    This module contains basics commands, which [user/file server] can using
+"""
+from json import dumps
+
+# available commands and handlers
+commands_user = {"AUTH": "autorization with server",
+                 "READ": "read some file from storage",
+                 "WRTE": "write file into storage",
+                 "DELT": "delete file from storage",
+                 "RNME": "rename file",
+                 "LIST": "get list of all files from storage with this user",
+                 "SYNC": "synchronize all files with storage on the server",
+                 "EXIT": "disconnect from server or end session"}
+
+commands_handlers_user   = dict((key, None) for key in commands_user.keys())
+commands_handlers_server = dict((key, None) for key in commands_user.keys() if key not in ('AUTH', 'EXIT'))
+
+
+def constructBasicJSON():
+    """
+        Create basic JSON for operations
+    """
+    data = {}
+    data['cmd'] = None
+    data['user'] = None
+    data['password'] = None
+    data['auth'] = None
+    data['error'] = None
+    return data
+
+
+def constructDataClient(cmd, user, hash_password, auth, error=''):
+    """
+        Create JSON for server from Client
+    """
+    data = constructBasicJSON()
+    data['cmd'] = cmd
+    data['user'] = user
+    data['password'] = hash_password
+    data['auth'] = auth
+    data['error'] = error
+    return dumps(data)
+
+
+def constructDataFileServer(cmd, user, hash_password, auth, error='', server_info=()):
+    """
+        Create JSON for server from File Server
+    """
+    data = constructBasicJSON()
+    data['cmd'] = cmd
+    data['user'] = user
+    data['password'] = hash_password
+    data['auth'] = auth
+    data['error'] = error
+    data['server'] = server_info
+    return dumps(data)
+
+
+def AUTH(result, data):
+    error_msg = "[AUTH] User=%s successfully logged..." % data['user']
+    # user not found at DB
+    if result is None:
+        data['cmd'] = 'RAUT'
+        data['error'] = 'User not found'
+        error_msg = "[AUTH] User=%s not found" % data['user']
+    else:
+        if result['name'] == data['user']:
+            # correct users info --> real user
+            if result['password'] == data['password']:
+                data['cmd'] = 'HELP'
+                data['auth'] = True
+            # incorrect password --> fake user
+            else:
+                data['cmd'] = 'RAUT'
+                data['error'] = 'Incorrect password. Try again...'
+                error_msg = "[AUTH] Incorrect password for user=%s" % data['user']
+    return data, error_msg
