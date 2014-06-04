b28872bcff43e775bb9717b9c309d06d619b632f
Valery Savich <Relrin78@gmail.com>
2014-06-04 18:06:31 +0300

    Refactoring code and add rsync integration

--- helenae/fileserver/fileserver_client.py
+++ helenae/fileserver/fileserver_client.py
@@ -1,9 +1,12 @@
 import json
 import sys
 import os
+from ast import literal_eval
 
 from twisted.internet import reactor
 from autobahn.twisted.websocket import WebSocketClientFactory, WebSocketClientProtocol, connectWS
+
+import rsync
 
 CONFIG_TEMPLATE = ''
 CONFIG_DATA = {}
@@ -19,39 +22,96 @@ class MessageBasedClientProtocol(WebSocketClientProtocol):
             other - some data
     """
     def onOpen(self):
+        self.file_1 = None  # patched file
+        self.file_2 = None  # file after receiving `delta`
+        self.delta_sync = None # changes in files as delta
+        self.result_cmd = ""
+        self.commands_handlers = self.__initHandlersUser()
         user_id = CONFIG_DATA['user']
         operation_name = CONFIG_DATA['cmd']
         file_id = CONFIG_DATA['file_id']
         src_file = CONFIG_DATA['src_file']
         data = '[' + str(user_id) + ':' + str(operation_name) + ':' + str(file_id) + ']'
+        # get data for write operations
         if operation_name == 'WRITE_FILE':
             with open(src_file, "r") as f:
                 info = f.read()
             data += str(info)
+        # or try to get information for sync operations
+        elif operation_name == 'RSYNC_FILE':
+            self.file_1 = open(src_file, "rb")
+            hashes = rsync.blockchecksums(self.file_1)
+            data += str(hashes)
+        self.sendMessage(data, isBinary=True)
+
+    def __initHandlersUser(self):
+        handlers = {}
+        handlers['WRT'] = self.print_payload
+        handlers['DEL'] = self.print_payload
+        handlers['REA'] = self.read_command
+        handlers['RSY'] = self.read_sync
+        handlers['WSY'] = self.write_sync
+        handlers['SYC'] = self.print_payload
+        return handlers
+
+    def stop_and_delete_config(self):
+        os.remove(CONFIG_TEMPLATE)
+        reactor.stop()
+
+    def print_payload(self, payload):
+        print payload
+        self.stop_and_delete_config()
+
+    def read_command(self, payload):
+        if self.result_cmd == 'C':
+            try:
+                data = payload[8:]
+                f = open(CONFIG_DATA['src_file'], "wb")
+                f.write(data)
+            except IOError, e:
+                print e
+            except Exception, e:
+                raise Exception(e)
+            finally:
+                print payload[:8] + "Successfully!"
+                f.close()
+        else:
+            self.print_payload(payload)
+        self.stop_and_delete_config()
+
+    def read_sync(self, payload):
+        self.delta_sync = literal_eval(payload[8:])
+        self.file_1.seek(0)
+        path_parts = CONFIG_DATA['src_file'].split('/')
+        path_parts[-1] = 'swap-' + path_parts[-1]
+        full_swap_filepath = '/'.join(path_parts)
+        self.file_2 = open(full_swap_filepath, "wb")
+        rsync.patchstream(self.file_1, self.file_2, self.delta_sync)
+        self.file_1.close()
+        self.file_2.close()
+        os.remove(CONFIG_DATA['src_file'])
+        os.rename(full_swap_filepath, CONFIG_DATA['src_file'])
+        self.file_1 = self.file_2 = self.delta_sync = None
+        self.stop_and_delete_config()
+
+    def write_sync(self, payload):
+        hashes = literal_eval(payload[8:])
+        data = '[' + str(CONFIG_DATA['user']) + ':' + "WCSYN_FILE" + ':' + str(CONFIG_DATA['file_id']) + ']'
+        try:
+            patchedfile = open(CONFIG_DATA['src_file'], "rb")
+            data += str(rsync.rsyncdelta(patchedfile, hashes))
+        except IOError, argument:
+            print argument.message
+        except Exception, argument:
+            print argument.message
+            raise Exception(argument)
         self.sendMessage(data, isBinary=True)
 
     def onMessage(self, payload, isBinary):
         cmd = payload[1:4]
-        result_cmd = payload[6]
-        if cmd in ('WRT', 'DEL'):
-            print payload
-        elif cmd == 'REA':
-            if result_cmd == 'C':
-                try:
-                    data = payload[8:]
-                    f = open(CONFIG_DATA['src_file'], "wb")
-                    f.write(data)
-                except IOError, e:
-                    print e
-                except Exception, e:
-                    raise Exception(e)
-                finally:
-                    print payload[:8] + "Successfully!"
-                    f.close()
-            else:
-                print payload
-        os.remove(CONFIG_TEMPLATE)
-        reactor.stop()
+        self.result_cmd = payload[6]
+        if cmd in ('WRT', 'REA', 'DEL', 'RSY', 'WSY', 'SYC'):
+            self.commands_handlers[cmd](payload)
 
 
 if __name__ == '__main__':
