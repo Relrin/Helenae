acfc39cfdbe8aae74d0a08a174611c0ee976cfb7
Valery Savich <Relrin78@gmail.com>
2014-07-08 11:08:30 +0300

    Fixture for twisted server

---
+++ tests/fixtures/twisted.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+import subprocess
+
+
+@pytest.yield_fixture
+def twisted_server(scope="module", autouse=True):
+    """
+        Using fixture for running server in other process
+    """
+    twisted_server_cmd = ['python'
+                         ,'../../helenae/server.py'
+                         ,'9000'
+    ]
+    twisted_server_proc = subprocess.Popen(twisted_server_cmd
+                                          ,stderr=open(os.devnull)
+                                          ,stdout=open(os.devnull)
+                                          ,shell=False
+    )
+    yield twisted_server_proc.communicate()[0]
+    twisted_server_proc.terminate()
