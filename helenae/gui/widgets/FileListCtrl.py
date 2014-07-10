a5ecf4083ba689e47ebc07657535e0716a2e1e43
Valery Savich <Relrin78@gmail.com>
2014-07-11 01:32:26 +0300

    Added function for commands: convert Size

--- helenae/gui/widgets/FileListCtrl.py
+++ helenae/gui/widgets/FileListCtrl.py
@@ -6,6 +6,7 @@
 import wx
 import os
 import time
+from helenae.utils.commands import convertSize
 
 
 class FileListCtrl(wx.ListCtrl):
@@ -13,6 +14,7 @@ class FileListCtrl(wx.ListCtrl):
     def __init__(self, parent, id, ico_folder):
         wx.ListCtrl.__init__(self, parent, id, style=wx.LC_REPORT)
 
+        self.currentDir = None
         self.__path = ico_folder + '/icons/mimetypes/'
         images = [ico_folder + '/icons/empty.png', ico_folder + '/icons/mimetypes/folder.png', ico_folder + '/icons/ui/up16.png']
         types_icons = [self.__path + f for f in os.listdir(self.__path)]
@@ -36,41 +38,55 @@ class FileListCtrl(wx.ListCtrl):
             self.il.Add(wx.Bitmap(i))
         self.SetImageList(self.il, wx.IMAGE_LIST_SMALL)
 
-        j = 0
-        # up from this folder to '..'
-        #self.InsertStringItem(0, '..')
-        #self.SetItemImage(0, 2)
+        # events
+        self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.onLeftClick)
+
+    def setCurrentDir(self, dir):
+        self.currentDir = dir
+        if not self.currentDir.endswith('/'):
+            self.currentDir += '/'
+
+
+    def insertUpDirectory(self):
+        #up from this folder to '..'
+        self.InsertStringItem(0, '..')
+        self.SetItemImage(0, 2)
+
+    def showFilesInDirectory(self, directory='.', j=0):
+        self.DeleteAllItems()
+
+        # add at the and of our 'directory' string '/' if doesn't contain this symbol
+        if not directory.endswith('/'):
+            directory += '/'
 
         # all founded folders append first
-        directories = [f for f in os.listdir('.') if os.path.isdir(f)]
+        directories = [f for f in os.listdir(directory) if os.path.isdir(directory+f)]
         directories.sort()
-        directories = directories
 
         for i in directories:
-            (name, ext) = os.path.splitext(i)
-            ex = ext[1:]
-            size = os.path.getsize(i)
-            sec = os.path.getmtime(i)
-            create_time = os.path.getctime(i)
-            self.InsertStringItem(j, name)
-            self.SetStringItem(j, 1, ex)
-            self.SetStringItem(j, 2, str(size) + ' B')
+            size = os.path.getsize(directory+i)
+            sec = os.path.getmtime(directory+i)
+            self.InsertStringItem(j, i)
+            self.SetStringItem(j, 1, '')
+            self.SetStringItem(j, 2, '')
             self.SetStringItem(j, 3, time.strftime('%Y-%m-%d %H:%M', time.localtime(sec)))
             self.SetItemImage(j, 1)
+            if (j % 2) == 0:
+                self.SetItemBackgroundColour(j, '#e6f1f5')
             j += 1
 
         # after append all files in directory
-        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        files = [f for f in os.listdir(directory) if os.path.isfile(directory+f)]
         files.sort()
 
         for i in files:
             (name, ext) = os.path.splitext(i)
             ex = ext[1:]
-            size = os.path.getsize(i)
-            sec = os.path.getmtime(i)
+            size = os.path.getsize(directory+i)
+            sec = os.path.getmtime(directory+i)
             self.InsertStringItem(j, name)
             self.SetStringItem(j, 1, ex)
-            self.SetStringItem(j, 2, str(size) + ' B')
+            self.SetStringItem(j, 2, convertSize(size))
             self.SetStringItem(j, 3, time.strftime('%Y-%m-%d %H:%M', time.localtime(sec)))
 
             # its supported type
@@ -90,3 +106,23 @@ class FileListCtrl(wx.ListCtrl):
         self.SetColumnWidth(1, sizeX * 0.1)
         self.SetColumnWidth(2, sizeX * 0.1875)
         self.SetColumnWidth(3, sizeX * 0.15)
+
+    def onLeftClick(self, event):
+        index = self.GetFirstSelected()
+        item = self.getFullnameItem(index)
+        # TODO: go to some folder inside...
+
+    def getFullnameItem(self, index):
+        item = self.GetItem(index, 0).GetText()
+        if self.GetItem(index, 1).GetText() != '':
+            item += '.' + self.GetItem(index, 1).GetText()
+        return item
+
+    def getSelectedItems(self):
+        selection = []
+        index = self.GetFirstSelected()
+        selection.append(self.getFullnameItem(index))
+        while len(selection) != self.GetSelectedItemCount():
+            index = self.GetNextSelected(index)
+            selection.append(self.getFullnameItem(index))
+        return selection
